9659a701a66e045a4d0d44bacae908362df7226a
Grigoriy Legal <grigoriylegal@yandex.ru>
2023-03-26 13:48:49 +0300

    седьмой семинар и домашка

--- src/main/java/org/example/Seminar6hw/view/View.java
+++ src/main/java/org/example/Seminar6hw/view/View.java
@@ -1,2 +1,9 @@
-package org.example.Seminar6hw.view;public class View {
+package org.example.Seminar6hw.view;
+
+import org.example.Seminar6hw.service.PresrnterService;
+
+public interface View {
+    void setPresenter(PresrnterService presenter);
+    void start();
+    void print(String text);
 }
